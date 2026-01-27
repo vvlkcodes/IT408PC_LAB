c79e74ff003794e75f2dd0d5100bc457298d9707
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2026-01-27 13:49:27 +0530

    Create ControlStatements.java

---
+++ LAB_1/ControlStatements.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+public class ControlStatements{
+   public static void main(String[] args){
+
+	int num1 = 10;
+	int num2 = 20;
+	float num3 = (int)num1;
+	char ch = 'A';
+	boolean variable = true;
+	if (num1>num2){
+	variable = false;
+	}
+	String str = "Java Programming";
+	String str1 = new String("Java Programming");
+	System.out.println(variable+ " " + str+ " " +num1 + " " +num2 + " " +num3 + " "+ch);
+
+       }
+}
