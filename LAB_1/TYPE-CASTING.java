635055ef23e7d033623145259c6765458f1eeb18
V.Venkata Lalith Kartheek <vuppulurilalith@gmail.com>
2026-01-27 13:47:48 +0530

    Create TYPE-CASTING.java

---
+++ LAB_1/TYPE-CASTING.java
@@ -0,0 +1,14 @@
+ class First{
+   public static void main(String[] args){
+        int x=10;
+      	double y=x;
+      	float z=(float)y;
+      	long num=400;
+      	int a=(int)num;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(num);
+        System.out.println(z);
+        System.out.println(a);
+    }
+}
